9dfa2863644bc55290f10fa5dbd70969570a5555
s2422980 <y.budnyk789@gmail.com>
2024-01-30 10:37:46 +0100

    Fixed the images of the brands/clients

--- src/assets/logos/CMLogo.jsx
+++ src/assets/logos/CMLogo.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import newLogo from '../images/cm_logo.svg'; // Adjust the path as necessary
+import newLogo from '../images/cm_logo.svg'; 
 
 
 export const CreativeMulesLogo = () => (
-<img 
-    src={newLogo} 
-    alt="Creative Mules Logo" 
-    width="200" 
+<svg width="200" 
     height="60" 
-    style={{ filter: 'grayscale(100%) brightness(60%) contrast(90%)' }}  
-  />
+    style={{ filter: 'grayscale(100%) brightness(60%) contrast(90%)' }}  viewBox="0 0 926 260" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect x="463" y="-0.000198364" width="462.999" height="260" fill="black"/>
+<ellipse cx="231.499" cy="130" rx="231.499" ry="130" fill="black"/>
+</svg> 
 );
