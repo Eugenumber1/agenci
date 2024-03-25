7f32adbe68705942be3fc960c2330f269a394b1c
s2422980 <y.budnyk789@gmail.com>
2024-03-25 12:39:52 +0100

    Update Dima Photo

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,7 +6,7 @@ import { CreativeMulesLogo } from "../assets/logos/CMLogo";
 
 const teamMembers = [
     {name: "Yevhenii Budnyk", role: "Full-Stack Developer and AI Engineer", image: "https://media.licdn.com/dms/image/C4E03AQFVTTP3y9GYmQ/profile-displayphoto-shrink_200_200/0/1626444215132?e=1714608000&v=beta&t=71kVyu9YGj3q0ddUdQxyb9c7GirI-xdGD0g4QxG_Z-w", linkedin: "https://www.linkedin.com/in/yevhenii-budnyk/"},
-    {name: "Dmytro Druzhniev", role: "Full-Stack Developer and System Architect", image: "https://media.licdn.com/dms/image/D4E35AQGmkBh9dcv0ZQ/profile-framedphoto-shrink_400_400/0/1704978014146?e=1709748000&v=beta&t=-gR8QSbWImj-oss-R8D9XsyauWYwHOLcUAo9j0s5Zls", linkedin: "https://www.linkedin.com/in/dmytro-druzhniev/"},
+    {name: "Dmytro Druzhniev", role: "Full-Stack Developer and System Architect", image: "https://media.licdn.com/dms/image/D4E35AQGmkBh9dcv0ZQ/profile-framedphoto-shrink_400_400/0/1704978014146?e=1711972800&v=beta&t=1tFHth3VBOYsM5a0JyoEn5g7bxiIP6aPYCbbDaT4s4k", linkedin: "https://www.linkedin.com/in/dmytro-druzhniev/"},
     {name: "Fausto Albers", role: "Innovator, AI and Business Developer", image: "https://media.licdn.com/dms/image/D4E03AQGhuHR5FE_dEw/profile-displayphoto-shrink_400_400/0/1675897868757?e=1714608000&v=beta&t=5P0_kdSRE6MWusqYcvzREFWREqXdJTOJMsjH3Td1m8k", linkedin: "https://www.linkedin.com/in/stepintoliquid/"},
     {name: "Simona Cojocaru", role: "UI/UX Designer", image: "https://media.licdn.com/dms/image/D4E03AQG7lHsjVUUGtg/profile-displayphoto-shrink_200_200/0/1706481123407?e=1714608000&v=beta&t=5PzAU8RwwcvI_vS_YyEkXPzlzR-HctG9Vt6VtrL-IXc", linkedin: "https://www.linkedin.com/in/simona-cojocaru01/"},
 ]
